b6bc9f944df28fe60ba33b2e9ec4fe11b94a6c14
llghdud921 <llghdud921@naver.com>
2021-12-15 02:28:27 +0900

    이전에 공부했던 프로젝트 추가

---
+++ awakeFromNib/awakeFromNib/ViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: customLabel!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+
+}
+
