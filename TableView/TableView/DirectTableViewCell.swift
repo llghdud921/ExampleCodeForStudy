511c1d83c77d2ff607b65b268e5b7afd99a35278
llghdud921 <llghdud921@naver.com>
2021-12-16 00:31:17 +0900

    tableView 수정

--- TableView/TableView/DirectTableViewCell.swift
+++ TableView/TableView/DirectTableViewCell.swift
@@ -7,16 +7,24 @@ class DirectTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(testLabel)
-        
-        testLabel.text = "test"
+        autolayout()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    func autolayout() {
+        // autoResizing
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(
+            [
+                testLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+                testLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 10),
+                testLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+                testLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 10)
+            ]
+        )
     }
-
 }
--- TableView/TableView/ViewController.swift
+++ TableView/TableView/ViewController.swift
@@ -8,12 +8,31 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.delegate = self
         tableView.dataSource = self
         
         view.addSubview(tableView)
+        
+        autolayout()
+        
         tableView.register(DirectTableViewCell.self, forCellReuseIdentifier: directIdentifier)
     }
+    
+    func autolayout() {
+        // autoResizing
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(
+            [
+                tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+                tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+                tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+            ]
+        )
+    }
+
 }
 
 // MARK: - tabelView datasource
@@ -27,6 +46,8 @@ extension ViewController {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: directIdentifier, for: indexPath) as? DirectTableViewCell else {
             fatalError()
         }
+        
+        cell.testLabel.text = "test"
         
         return cell
     }
